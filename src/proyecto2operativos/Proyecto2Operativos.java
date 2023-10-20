aacf27140f2f764b1941fcd906765d480fe0e657
Luis Pernía <86423815+Luispdiaz@users.noreply.github.com>
2023-10-20 12:59:49 -0400

    Commit Vacio

---
+++ src/proyecto2operativos/Proyecto2Operativos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2operativos;
+
+/**
+ *
+ * @author Luisp
+ */
+public class Proyecto2Operativos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        {}
+    }
+    
+}
